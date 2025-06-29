2a8bbd67460fe6813445126995ddd38fea2a8dc8
andersonteofilo <anderson.siquelli@gmail.com>
2025-06-28 21:44:33 -0300

    Adicionando crossorigin no health.

--- delivery-api/src/main/java/com/deliverytech/delivery/controller/HealthController.java
+++ delivery-api/src/main/java/com/deliverytech/delivery/controller/HealthController.java
@@ -3,10 +3,12 @@ package com.deliverytech.delivery.controller;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthController {
 
   @GetMapping("/health")
