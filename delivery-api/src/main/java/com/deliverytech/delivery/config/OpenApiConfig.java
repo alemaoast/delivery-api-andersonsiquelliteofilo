38d93028a379fdb601fa5701ce95adc4cf88bd72
andersonteofilo <anderson.siquelli@gmail.com>
2025-07-17 20:47:12 -0300

    Adicionando o scheme de autenticacao no swagger openapi.

--- delivery-api/src/main/java/com/deliverytech/delivery/config/OpenApiConfig.java
+++ delivery-api/src/main/java/com/deliverytech/delivery/config/OpenApiConfig.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
@@ -34,6 +37,18 @@ public class OpenApiConfig {
                 .description("Local Development Server"),
             new Server()
                 .url("https://api.deliverytech.com/v1")
-                .description("Production Server")));
+                .description("Production Server")))
+        .components(
+            new Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"))
+        )
+        .addSecurityItem(
+            new SecurityRequirement().addList("bearerAuth")
+        );
   }
 }
