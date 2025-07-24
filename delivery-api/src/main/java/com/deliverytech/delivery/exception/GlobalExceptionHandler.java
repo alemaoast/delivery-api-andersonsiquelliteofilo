ec46e26604870cb076200576fca185496934f38f
andersonteofilo <anderson.siquelli@gmail.com>
2025-07-23 22:41:05 -0300

    Adicionando log4j nas exceptions.

--- delivery-api/src/main/java/com/deliverytech/delivery/exception/GlobalExceptionHandler.java
+++ delivery-api/src/main/java/com/deliverytech/delivery/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.hibernate.TransactionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,53 +22,55 @@ import jakarta.validation.ValidationException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+        private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
         @ExceptionHandler(Exception.class)
-        public ResponseEntity<ErrorResponseDTO> handleGenericException(
-                        Exception ex, WebRequest request) {
+        public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception ex,
+                        WebRequest request) {
 
                 ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                                "Erro interno do servidor",
-                                "Ocorreu um erro inesperado",
+                                "Erro interno do servidor", "Ocorreu um erro inesperado",
                                 ((ServletWebRequest) request).getRequest().getRequestURI());
 
-                                return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-                //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+                logger.error("{}{}", errorResponse.getPath(), errorResponse.getMessage());
+
+                return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         @ExceptionHandler(EntityNotFoundException.class)
         public ResponseEntity<ErrorResponseDTO> handleEntityNotFoundException(
                         EntityNotFoundException ex, WebRequest request) {
 
-                ErrorResponseDTO errorResponse = new ErrorResponseDTO(
-                                HttpStatus.NOT_FOUND.value(),
-                                "Não encontrado",
-                                ex.getMessage(),
+                ErrorResponseDTO errorResponse = new ErrorResponseDTO(HttpStatus.NOT_FOUND.value(),
+                                "Não encontrado", ex.getMessage(),
                                 ((ServletWebRequest) request).getRequest().getRequestURI());
+
+                logger.error("{}{}", errorResponse.getPath(), errorResponse.getMessage());
 
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
 
         @ExceptionHandler(ConflictException.class)
-        public ResponseEntity<ErrorResponseDTO> handleConflictException(
-                        ConflictException ex, WebRequest request) {
+        public ResponseEntity<ErrorResponseDTO> handleConflictException(ConflictException ex,
+                        WebRequest request) {
 
-                ErrorResponseDTO errorResponse = new ErrorResponseDTO(
-                                HttpStatus.CONFLICT.value(),
-                                "Conflito",
-                                ex.getMessage(),
+                ErrorResponseDTO errorResponse = new ErrorResponseDTO(HttpStatus.CONFLICT.value(),
+                                "Conflito", ex.getMessage(),
                                 ((ServletWebRequest) request).getRequest().getRequestURI());
+
+                logger.error("{}{}", errorResponse.getPath(), errorResponse.getMessage());
 
                 return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
         }
 
         @ExceptionHandler(BusinessException.class)
-        public ResponseEntity<ValidationErrorResponse> handleBusinessException(BusinessException ex) {
+        public ResponseEntity<ValidationErrorResponse> handleBusinessException(
+                        BusinessException ex) {
                 ValidationErrorResponse error = new ValidationErrorResponse(
-                                HttpStatus.BAD_REQUEST.value(),
-                                "Erro de regra de negócio",
-                                ex.getMessage(),
-                                LocalDateTime.now());
+                                HttpStatus.BAD_REQUEST.value(), "Erro de regra de negócio",
+                                ex.getMessage(), LocalDateTime.now());
+
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
         }
 
@@ -75,12 +79,11 @@ public class GlobalExceptionHandler {
                         MethodArgumentNotValidException ex, WebRequest request) {
 
                 Map<String, String> details = new HashMap<>();
-                ex.getBindingResult().getFieldErrors()
-                                .forEach(error -> details.put(error.getField(), error.getDefaultMessage()));
+                ex.getBindingResult().getFieldErrors().forEach(
+                                error -> details.put(error.getField(), error.getDefaultMessage()));
 
                 ErrorResponseDTO errorResponse = new ErrorResponseDTO(
-                                HttpStatus.BAD_REQUEST.value(),
-                                "Erro de validação",
+                                HttpStatus.BAD_REQUEST.value(), "Erro de validação",
                                 "Campos inválidos na requisição",
                                 ((ServletWebRequest) request).getRequest().getRequestURI(),
                                 details);
@@ -89,22 +92,20 @@ public class GlobalExceptionHandler {
         }
 
         @ExceptionHandler(ValidationException.class)
-        public ResponseEntity<ValidationErrorResponse> handleValidationException(ValidationException ex) {
+        public ResponseEntity<ValidationErrorResponse> handleValidationException(
+                        ValidationException ex) {
                 ValidationErrorResponse error = new ValidationErrorResponse(
-                                HttpStatus.BAD_REQUEST.value(),
-                                "Erro de dados inválidos",
-                                ex.getMessage(),
-                                LocalDateTime.now());
+                                HttpStatus.BAD_REQUEST.value(), "Erro de dados inválidos",
+                                ex.getMessage(), LocalDateTime.now());
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
         }
 
         @ExceptionHandler(TransactionException.class)
-        public ResponseEntity<ValidationErrorResponse> handleTransactionException(TransactionException ex) {
+        public ResponseEntity<ValidationErrorResponse> handleTransactionException(
+                        TransactionException ex) {
                 ValidationErrorResponse error = new ValidationErrorResponse(
-                                HttpStatus.BAD_REQUEST.value(),
-                                "Erro transacional",
-                                ex.getMessage(),
-                                LocalDateTime.now());
+                                HttpStatus.BAD_REQUEST.value(), "Erro transacional",
+                                ex.getMessage(), LocalDateTime.now());
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
         }
 }
